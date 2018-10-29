51c2e39e567daeabb50672073ad544ed88d1714f
guangdazhou <1793943232@qq.com>
2018-10-29 06:05:52 -0700

    fix 2

--- first_project/App/urls.py
+++ first_project/App/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from App import views
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$',views.index,name='index'),
-}
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+]
--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,5 +1,78 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'index.html')
+    username = request.session.get('username')
+
+    return render(request,'index.html',context={'username':username})
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        tel = request.POST.get('tel')
+
+        # 存入数据库
+        try:
+            user = User()
+            user.username = username
+            user.password = password
+            user.tel = tel
+            user.save()
+
+            request.session['username'] = username
+            request.session.set_expiry(10)
+
+
+            return redirect('app:index')
+
+        except Exception as e:
+            return HttpResponse('注册失败' + e)
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        # 验证
+        password = request.POST.get('password')
+
+        users = User.objects.filter(username=username, password=password)
+        if users.exists():
+
+            user = users.first()
+
+            # 状态保持 - 设置cookie
+            # 状态保持 - 设置session
+            request.session['username'] = username
+            request.session.set_expiry(60)
+
+
+            return redirect('app:index')
+        else:
+            return HttpResponse('用户名或密码错误')
+
+def logout(request):
+    response = redirect('app:index')
+
+    ## session
+    # 方式一: session是借助于cookie
+    # response.delete_cookie('sessionid')
+
+    # 方式二: 直接删除session存储
+    del request.session['username']
+
+    # 方式三: 同时删除cookie和session
+    # request.session.flush()
+
+
+
+    return response
